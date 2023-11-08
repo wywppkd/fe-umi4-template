56c95c6f9697bf7cc44c32aecb3ae8f0b1e49141
Bran <1055025755@qq.com>
2023-11-08 14:44:43 +0800

    test lint

--- src/utils/sleep.ts
+++ src/utils/sleep.ts
@@ -1,4 +1,5 @@
 export default function sleep(duration: number = 1500): Promise<void> {
+  console.log('🚀 ~ file: sleep.ts:2 ~ sleep ~ duration:', duration);
   return new Promise((resolve) => {
     setTimeout(resolve, duration);
   });
